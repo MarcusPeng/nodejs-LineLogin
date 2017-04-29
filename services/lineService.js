1d28203c8e33812d9b09a53351024f5a14415f0a
Marcus Peng <ms855187@gmail.com>
2017-04-29 17:34:53 +0800

    change application info to dynamic

--- services/lineService.js
+++ services/lineService.js
@@ -4,30 +4,31 @@ var notifydb = require('../repository/notifydb');
 
 const lineService = {
     getAccessUrl: function(callback) {
-        notifydb.getApplicationInfo(function(item) {
+        notifydb.getApplicationInfo(function(applicationInfo) {
             const accessUrl = config.lineApi.weblogin + 
                 "?response_type=code" +
-                "&client_id=" + item.channelId + 
-                "&redirect_uri=" + item.CallbackUrl +
+                "&client_id=" + applicationInfo.channelId + 
+                "&redirect_uri=" + applicationInfo.CallbackUrl +
                 "&state=" + config.lineApi.state;
             callback && callback(accessUrl);
         });
     },
 
     getAccessToken: function(code, callback) {
-        let options = {
-            url: config.lineApi.accessToken,
-            form: { 
-                grant_type:'authorization_code',
-                client_id: '1512396874',
-                client_secret: '4801d29a5f8bf7549316bdf535ecfb01',
-                code: code,
-                redirect_uri: 'http://localhost:3000/auth/line-callback'
-            }
-        };
-
-        request.post(options, function(error, response, body) {
-            callback && callback(error, response, body);
+        notifydb.getApplicationInfo(function(applicationInfo) {
+            let options = {
+                url: config.lineApi.accessToken,
+                form: { 
+                    grant_type:'authorization_code',
+                    client_id: applicationInfo.channelId,
+                    client_secret: applicationInfo.channelSecret,
+                    code: code,
+                    redirect_uri: applicationInfo.CallbackUrl
+                }
+            };
+            request.post(options, function(error, response, body) {
+                callback && callback(error, response, body);
+            });
         });
     },
 
